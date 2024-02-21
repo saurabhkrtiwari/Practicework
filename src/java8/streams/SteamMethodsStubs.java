0f0a0d3df43693a4b5046a4ab1df943f528dcfc5
saurabhkrtiwari <saurabhkrtiwari@gmail.com>
2024-02-21 15:27:34 +0530

    Given a list of strings, find the longest string.

--- src/java8/streams/SteamMethodsStubs.java
+++ src/java8/streams/SteamMethodsStubs.java
@@ -1,13 +1,20 @@
 package java8.streams;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class SteamMethodsStubs {
 
-        public static void findAvg(List<Integer> list) {
+    public static void findAvg(List<Integer> list) {
         OptionalDouble average = list.stream().mapToInt(value -> value).average();
         if(average.isPresent()){
             System.out.println("Average "+average.getAsDouble());
         }
+    }
+
+    public static void findLongestString(List<String> listOfString) {
+        String s = listOfString.stream().max(Comparator.comparingInt(String::length)).orElse(null);
+        assert s != null;
+        System.out.println("Longest String "+s + " for length "+s.length());
     }
 }
